f4bce63ab76cc2e4ae0714cb75ecd9ebee9dbdd6
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2025-01-31 15:02:33 -0700

    Finished game mechanics

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -4,6 +4,9 @@ var pointer
 var data
 var selection
 var airportSel
+var fade = false
+
+var crt = true
 
 func _ready():
 	var icon = Texture2D.new()
@@ -13,22 +16,32 @@ func _ready():
 	var purchase = $tabs/purchase
 	
 	$tabs/overview/container/name.text = data.name
-	print(data.name)
 	
 	$tabs/purchase/scroll/purchaseList.fixed_icon_size = Vector2(512, 218)
 	for aircraft in data.aircraft:
 		$tabs/purchase/scroll/purchaseList.add_item(aircraft.name,load("res://sprites/" + str(aircraft.code) + ".png"))
-	
+		
+	for aircraft in data.aircraft:
+		fly(aircraft)
+
 func _process(delta):
-	pass
-	
+	if crt:
+		$crt.show()
+	else:
+		$crt.hide()
+		
+	if fade:
+		$fade.color.a += delta*1.5
+	elif not fade and $fade.color.a > 0:
+		$fade.color.a -= delta*1.5
+
 func _input(event):
 	if event is InputEventMouse:
 		pointer = event.position
 
 func _on_update_timeout():
 	var label
-	var grid = $tabs/overview/container/grid
+	var grid = $tabs/overview/container/hboxGrid/grid
 	grid.columns = 2
 	$tabs/routes/scroll/airportList.clear()
 	
@@ -38,7 +51,17 @@ func _on_update_timeout():
 		else:
 			$tabs/routes/scroll/airportList.add_item("   " + airport.code)
 	
-	for child in $tabs/overview/container/grid.get_children():
+	for child in $tabs/overview/container/hboxGrid/grid2.get_children():
+		child.queue_free()
+	
+	for airport in data.airports:
+		if airport.slot:
+			label = Label.new()
+			label.add_theme_font_size_override("font_size", 42)
+			label.text = airport.name
+			$tabs/overview/container/hboxGrid/grid2.add_child(label)
+	
+	for child in $tabs/overview/container/hboxGrid/grid.get_children():
 		child.queue_free()
 	
 	for item in data.aircraft:
@@ -47,32 +70,42 @@ func _on_update_timeout():
 		
 		name_label.add_theme_font_size_override("font_size", 42)
 		amount_label.add_theme_font_size_override("font_size", 42)
+		amount_label.add_theme_color_override("font_color", Color(1,1,0.5,1))
 		
 		grid.add_child(name_label)
 		grid.add_child(amount_label)
 		
 		name_label.text = "  " + item.name + "  "
 		amount_label.text = "  " + str(item.amount) + "  "
-		
-		if data.money >= 1000000:
-			$tabs/purchase/money.text = "$" + str(data.money/1000000) + "M"
-		elif data.money >= 10000:
-			$tabs/purchase/money.text = "$" + str(data.money/1000) + "K"
-		
-		if data.money >= 1000000:
-			$tabs/routes/money.text = "$" + str(data.money/1000000) + "M"
-		elif data.money >= 10000:
-			$tabs/routes/money.text = "$" + str(data.money/1000) + "K"
-			
-		if data.money >= 1000000:
-			$tabs/overview/money.text = "$" + str(data.money/1000000) + "M"
-		elif data.money >= 10000:
-			$tabs/overview/money.text = "$" + str(data.money/1000) + "K"
-		
+	if data.money >= 1000000000:
+		$tabs/purchase/moneyContainer/money.text = "    $" + str(data.money/1000000000) + "B"
+		$tabs/routes/moneyContainer/money.text = "    $" + str(data.money/1000000000) + "B"
+		$tabs/overview/moneyContainer/money.text = "    $" + str(data.money/1000000000) + "B"
+	elif data.money >= 1000000:
+		$tabs/purchase/moneyContainer/money.text = "    $" + str(data.money/1000000) + "M"
+		$tabs/routes/moneyContainer/money.text = "    $" + str(data.money/1000000) + "M"
+		$tabs/overview/moneyContainer/money.text = "    $" + str(data.money/1000000) + "M"
+	elif data.money >= 10000:
+		$tabs/purchase/moneyContainer/money.text = "    $" + str(data.money/1000) + "K"
+		$tabs/routes/moneyContainer/money.text = "    $" + str(data.money/1000) + "K"
+		$tabs/overview/moneyContainer/money.text = "    $" + str(data.money/1000) + "K"
+	
+	if calcRate() >= 1000000:
+		$tabs/overview/moneyContainer/rate.text = "$" + str(int(calcRate()/1000000)) + "M /sec    ."
+		$tabs/purchase/moneyContainer/rate.text = "$" + str(int(calcRate()/1000000)) + "M /sec    ."
+		$tabs/routes/moneyContainer/rate.text = "$" + str(int(calcRate()/1000000)) + "M /sec    ."
+	elif calcRate() >= 1000:
+		$tabs/overview/moneyContainer/rate.text = "$" + str(int(calcRate()/1000)) + "K /sec    ."
+		$tabs/purchase/moneyContainer/rate.text = "$" + str(int(calcRate()/1000)) + "K /sec    ."
+		$tabs/routes/moneyContainer/rate.text = "$" + str(int(calcRate()/1000)) + "K /sec    ."
+	else:
+		$tabs/overview/moneyContainer/rate.text = "$" + str(int(calcRate())) + "/sec    ."
+		$tabs/purchase/moneyContainer/rate.text = "$" + str(int(calcRate())) + "/sec    ."
+		$tabs/routes/moneyContainer/rate.text = "$" + str(int(calcRate())) + "/sec    ."
 		Global.setSave(data)
 
-
 func _on_purchase_list_item_selected(index):
+	$click.play()
 	selection = index
 	var plane = data.aircraft[index]
 	$tabs/purchase/scroll/purchaseList.deselect_all()
@@ -95,10 +128,11 @@ func buy(index):
 		data.money -= data.aircraft[index].price
 		data.aircraft[index].amount += 1
 		$tabs/purchase/notice.popup_centered()
+		$select.play()
 	else:
-		$tabs/purchase/notice/text.text = "Not enough"
+		$tabs/purchase/notice/text.text = "\n \nNot enough"
 		$tabs/purchase/notice.popup_centered()
-
+		$click.play()
 
 func buySlot(index):
 	$tabs/routes/buySlot.hide()
@@ -107,24 +141,30 @@ func buySlot(index):
 		data.money -= data.airports[index].cost
 		data.airports[index].slot = true
 		$tabs/routes/notice.popup_centered()
+		$select.play()
 	else:
-		$tabs/routes/notice/text.text = "Not enough"
+		$tabs/routes/notice/text.text = "\n \nNot enough"
 		$tabs/routes/notice.popup_centered()
+		$click.play()
 
 func _on_buy_button_down():
+	$click.play()
 	print("buy button")
 	buy(selection)
 
 func _on_airport_list_item_selected(index):
+	$click.play()
 	var airport = data.airports[index]
 	$tabs/routes/buySlot/vbox/name.text = "\n" + airport.name
-	if airport.cost >= 1000000:
-			$tabs/routes/buySlot/vbox/cost.text = "$" + str(airport.cost/1000000) + "M"
-	elif airport.cost >= 10000:
-		$tabs/routes/buySlot/vbox/cost.text = "$" + str(airport.cost/1000) + "K"
-	$tabs/routes/buySlot.popup_centered()
 	$tabs/routes/buySlot/vbox/mult.text = str(airport.mult) + "x multiplier"
 	$tabs/routes/buySlot/vbox/code.text = airport.code
+	if not airport.slot:
+		$tabs/routes/buySlot/vbox/cost.text = "$" + str(airport.cost/1000000) + "M"
+		$tabs/routes/buySlot/vbox/buySlot.disabled = false
+	else:
+		$tabs/routes/buySlot/vbox/cost.text = "purchased"
+		$tabs/routes/buySlot/vbox/buySlot.disabled = true
+	$tabs/routes/buySlot.popup_centered()
 	airportSel = index
 
 func _on_name_text_changed(new_text):
@@ -132,15 +172,13 @@ func _on_name_text_changed(new_text):
 	Global.setSave(data)
 
 func _on_buy_slot_button_down():
+	$click.play()
 	buySlot(airportSel)
 
-func fly(aircraft,airport):
-	await get_tree().create_timer(1.8**data.airports[airport].distance).timeout
-	data.money += ((data.aircrafts[aircraft].baseRate * 500)*airport.mult)*aircraft.amount
-	fly(aircraft,airport)
 
 func _on_airports_button_down():
-	var range = data.aircraft[selection].maxRouteStyle
+	$click.play()
+	var range = data.aircraft[selection].range
 	$tabs/purchase/notice/text.text = ""
 	$tabs/purchase/buyMenu.popup_centered()
 	for airport in data.airports:
@@ -149,10 +187,51 @@ func _on_airports_button_down():
 	$tabs/purchase/notice.popup(Rect2i(pointer,Vector2i(200,150)))
 
 func _on_buy_menu_close_requested():
+	$click.play()
 	$tabs/purchase/notice.hide()
 
 func _on_buy_menu_go_back_requested():
+	$click.play()
 	$tabs/purchase/notice.hide()
 
 func getBestAirport(aircraft):
 	pass
+
+func fly(aircraft):
+	var airport = data.airports[1]
+	for airports in data.airports:
+		if airports.distance <= aircraft.range and airports.slot:
+			if airports.mult > airport.mult:
+				airport = airports
+	await get_tree().create_timer(1.8**airport.distance + clamp(aircraft.baseRate/100,2,6)).timeout
+	data.money += ((aircraft.baseRate * 500)*airport.mult)*aircraft.amount
+	if ((aircraft.baseRate * 500)*airport.mult)*aircraft.amount > 0:
+		print(str(((aircraft.baseRate * 500)*airport.mult)*aircraft.amount) + ", " + airport.code + ", " + aircraft.name)
+	fly(aircraft)
+
+func calcRate():
+	var total = 0
+	for aircraft in data.aircraft:
+		var airport = data.airports[1]
+		for airports in data.airports:
+			if airports.distance <= aircraft.range and airports.slot:
+				if airports.mult > airport.mult:
+					airport = airports
+		total += (((aircraft.baseRate * 500)*airport.mult)*aircraft.amount)/(1.8**airport.distance + clamp(aircraft.baseRate/100,2,6))
+	return total
+
+func _on_back_button_up():
+	$click.play()
+	_on_update_timeout()
+	fade = true
+	await get_tree().create_timer(1).timeout
+	get_tree().change_scene_to_file("res://Scenes/title.tscn")
+
+func _on_tabs_tab_clicked(tab):
+	$click.play()
+
+func _on_crt_toggle_button_up():
+	if crt:
+		crt = false
+	else:
+		crt = true
--- Scripts/title.gd
+++ Scripts/title.gd
@@ -1,14 +1,14 @@
 extends Control
 
 const baseSave = {
-	"name": "New Airline",
-	"money": 500000000,
+	"name": "",
+	"money": 50000000000,
 	"aircraft": [
 		{
 			"name": "Bombardier Q400",
 			"code": "q400",
 			"amount": 2,
-			"maxRouteStyle": 1,
+			"range": 1,
 			"baseRate": 200,
 			"price": 20000000
 		},
@@ -16,40 +16,40 @@ const baseSave = {
 			"name": "Bombardier CRJ900",
 			"code": "crj9",
 			"amount": 0,
-			"maxRouteStyle": 1,
-			"baseRate": 350,
+			"range": 1,
+			"baseRate": 400,
 			"price": 75000000
 		},
 		{
 			"name": "Airbus A321XLR",
 			"code": "a21x",
 			"amount": 0,
-			"maxRouteStyle": 2,
-			"baseRate": 500,
+			"range": 2,
+			"baseRate": 800,
 			"price": 140000000
 		},
 		{
 			"name": "Boeing 787-9",
 			"code": "b787",
 			"amount": 0,
-			"maxRouteStyle": 3,
-			"baseRate": 800,
+			"range": 3,
+			"baseRate": 1500,
 			"price": 300000000
 		},
 		{
 			"name": "Airbus A350-1000",
 			"code": "a350",
 			"amount": 0,
-			"maxRouteStyle": 4,
-			"baseRate": 1000,
+			"range": 4,
+			"baseRate": 2500,
 			"price": 350000000
 		},
 		{
-			"name": "Boeing 747-8i",
+			"name": "Boeing 747-400",
 			"code": "b747",
 			"amount": 0,
-			"maxRouteStyle": 3,
-			"baseRate": 2000,
+			"range": 3,
+			"baseRate": 4000,
 			"price": 400000000
 		}
 	],
@@ -59,15 +59,15 @@ const baseSave = {
 			"name": "San Francisco Int'l",
 			"slot": true,
 			"cost": 0,
-			"mult": 1.0,
+			"mult": 0,
 			"distance": 0
 		},
-				{
+		{
 			"code": "YYC",
 			"name": "Calgary Int'l",
-			"slot": false,
+			"slot": true,
 			"cost": 1000000,
-			"mult": 1.2,
+			"mult": 0.8,
 			"distance": 1
 		},
 		{
@@ -75,7 +75,7 @@ const baseSave = {
 			"name": "Vancouver Int'l",
 			"slot": false,
 			"cost": 2000000,
-			"mult": 1.4,
+			"mult": 1.2,
 			"distance": 1
 		},
 		{
@@ -83,7 +83,7 @@ const baseSave = {
 			"name": "Toronto Pearson Int'l",
 			"slot": false,
 			"cost": 7000000,
-			"mult": 1.4,
+			"mult": 1.5,
 			"distance": 2
 		},
 		{
@@ -91,23 +91,39 @@ const baseSave = {
 			"name": "Los Angeles Int'l",
 			"slot": false,
 			"cost": 10000000,
-			"mult": 1.8,
+			"mult": 2,
 			"distance": 1
+		},
+		{
+			"code": "JFK",
+			"name": "John F. Kennedy Int'l",
+			"slot": false,
+			"cost": 10000000,
+			"mult": 2.4,
+			"distance": 3
 		},
 		{
 			"code": "LHR",
 			"name": "London Heahrow Int'l",
 			"slot": false,
 			"cost": 50000000,
-			"mult": 2,
+			"mult": 3,
+			"distance": 3
+		},
+		{
+			"code": "FRA",
+			"name": "Frankfurt Int'l",
+			"slot": false,
+			"cost": 70000000,
+			"mult": 3.4,
 			"distance": 3
 		},
 		{
 			"code": "SIN",
 			"name": "Singapore Changi Int'l",
 			"slot": false,
-			"cost": 100000000,
-			"mult": 2.2,
+			"cost": 150000000,
+			"mult": 5,
 			"distance": 4
 		}
 	]
@@ -115,15 +131,20 @@ const baseSave = {
 
 const save_path = "user://flightbase/save.json"
 
+var fade = false
 var saveData
 
 func _ready():
 	pass
 
 func _process(delta):
-	pass
+	if fade:
+		$fade.color.a += delta*1.5
+	elif not fade and $fade.color.a > 0:
+		$fade.color.a -= delta*1.5
 
 func _on_start_button_up():
+	$select.play()
 	var dir = DirAccess.open("user://")
 	if not dir.dir_exists("flightbase"):
 		dir.make_dir("flightbase")
@@ -131,7 +152,6 @@ func _on_start_button_up():
 	if FileAccess.file_exists(save_path):
 		print("File found")
 		saveData = FileAccess.open(save_path, FileAccess.READ).get_as_text()
-		
 		saveData = JSON.parse_string(saveData)
 		
 	else:
@@ -146,11 +166,12 @@ func _on_start_button_up():
 		else:
 			print("Error creating save file")
 	Global.setSave(saveData)
-	await get_tree().create_timer(0.1).timeout
+	fade = true
+	await get_tree().create_timer(0.8).timeout
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
-
 func _on_reset_button_up():
+	$select.play()
 	var dir = DirAccess.open("user://")
 	if not dir.dir_exists("flightbase"):
 		dir.make_dir("flightbase")
